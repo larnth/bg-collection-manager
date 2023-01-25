3b83797019b47560cb476f7d8cb95039747cc01e
Caleb <chobson@firebend.com>
2023-01-25 10:52:26 -0600

    Getting started

--- src/components/nav/HeaderToolbar.js
+++ src/components/nav/HeaderToolbar.js
@@ -1,23 +1,109 @@
-import { Menu } from '@mui/icons-material';
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import React from 'react';
+import { Casino, Menu as MenuIcon } from '@mui/icons-material';
+import { AppBar, Box, Button, IconButton, MenuItem, Toolbar, Typography } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import React, { useState } from 'react';
+
+// make real pages array, perhaps in context
+const pages = ['My Collection', 'Randomizer', 'Find a Game'];
 
 export default function HeaderToolbar () {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    
+      const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+      };
+
     return (
         <AppBar position="static">
             <Toolbar>
-            <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-            >
-                <Menu />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Collection Manager
-            </Typography>
+                <Casino sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' },
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Collection Manager
+                </Typography>
+
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                    <MenuIcon />
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                    }}
+                    >
+                    {pages.map((page) => (
+                        <MenuItem key={page} onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">{page}</Typography>
+                        </MenuItem>
+                    ))}
+                    </Menu>
+                </Box>
+                <Casino sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    href=""
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'flex', md: 'none' },
+                    flexGrow: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Collection Manager
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page) => (
+                    <Button
+                        key={page}
+                        onClick={handleCloseNavMenu}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                        {page}
+                    </Button>
+                    ))}
+                </Box>
             </Toolbar>
         </AppBar>
     )
